7b269acac457a6af8c7cf5242a8ff7e8a8294861
Travis Umbel <Umbelth94@gmail.com>
2023-01-10 19:32:45 -0600

    Additional button functional
    Addition button works but can't add additional numbers

--- scripts.js
+++ scripts.js
@@ -1,17 +1,50 @@
 const display = document.querySelector('#display');
 const numButtons = document.querySelectorAll('.numbutt');
+const clearButton = document.querySelector('#clear');
+const plusButton = document.querySelector('#add');
+const equalsButton = document.querySelector('#equals');
+
+let operatorPressed = '';
+let firstNum =''; //Attempt to make firstNum a string that can be turned into a number later.
+let secondNum='';
+let isFirst = true;
+let result = 0;
 
 numButtons.forEach(button => {
     button.addEventListener('click',() => {
-        console.log(button.textContent);
+            display.textContent += button.textContent;
+            if(isFirst){
+                firstNum += button.textContent;
+                console.log('first number ' + firstNum);
+            } else {
+                secondNum += button.textContent;
+                console.log('second number ' + secondNum);
+            }
     })
 });
 
 function displayNumber(number){
     display.textContent = number;
-}
+};
 
+equalsButton.addEventListener('click', () => {
+   result = operate(operatorPressed,firstNum,+secondNum);
+   console.log(result);
+   display.textContent = result;   
+});
 
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+})
+
+// + button
+plusButton.addEventListener('click', () => { //does not add yet!  
+    firstNum = +firstNum; //converts number
+    display.textContent = '';
+    isFirst = false;
+    operatorPressed = 'add';
+    
+});
 function add(a,b){
     console.log('add ' + a + '+' + b + '=' + a+b);
     return a + b;
@@ -33,5 +66,14 @@ function divide(a,b){
 }
 
 function operate(operator, a, b){
-    return operator(a,b);
-}
+    if (operator == 'add'){
+        return add(a,b);
+    } else if (operator == 'subtract')
+    {
+        return subtract(a,b);
+    } else if (operator =='multiply'){
+        return multiply(a,b)
+    } else if (operator =='divide'){
+        return divide(a,b);
+    }
+};
