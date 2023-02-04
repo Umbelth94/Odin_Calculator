8bbc5c78b2c84ab664ca0c628d88361d80c4a1de
Travis Umbel <Umbelth94@gmail.com>
2023-02-04 15:56:14 -0600

    w

--- scripts.js
+++ scripts.js
@@ -317,15 +317,15 @@ function divide(a,b){
 function operate(operator, a, b){ 
     if (operator == 'add'){
         console.log('add' + a + '+' + b);
-        return Math.round((add(a,b)) * 1000) / 1000;
+        return Math.round((add(a,b)) * 10000) / 10000;
     } else if (operator == 'subtract'){
         console.log('subtract' + a + b);
-        return Math.round((subtract(a,b)) * 1000) / 1000;
+        return Math.round((subtract(a,b)) * 10000) / 10000;
     } else if (operator =='multiply'){
         console.log('multiply' + a + b);
-        return Math.round((multiply(a,b)) * 1000) / 1000;
+        return Math.round((multiply(a,b)) * 10000) / 10000;
     } else if (operator =='divide'){
         console.log('divide' + a + b);
-        return Math.round((divide(a,b)) * 1000) / 1000;
+        return Math.round((divide(a,b)) * 10000) / 10000;
     }
 };
